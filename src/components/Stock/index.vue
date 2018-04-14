9bd7138aa02649da7ecd5a984dee6b5ce0f196a4
Aaron Hakala <aaron.hakala@metropolia.fi>
2018-04-14 18:09:00 +0300

    create chart data on mount

--- src/components/Stock/index.vue
+++ src/components/Stock/index.vue
@@ -2,14 +2,15 @@
   <div class="stock container">
     <div class="col-md-5 col-lg-4">
       <back-button />
-      <h1>Data</h1>
-      <h2>Id: {{ $route.params.id }}</h2>
+      <h1>{{ title }}</h1>
+      <p>ID: <strong>{{ $route.params.id }}</strong></p>
+      <br>
       <div v-for="name in names">
         <button class="btn btn-info" v-bind:key="name" v-on:click="() => changeName(name)">{{ name }}</button>
       </div>
     </div>
     <div class="col-md-7 col-lg-8">
-      <line-chart v-if="selectedName" :chart-data="selectedData" :options="chartOptions" />
+      <line-chart v-if="selectedData" :chart-data="selectedData" :options="chartOptions" />
     </div>
   </div>
 </template>
@@ -18,78 +19,80 @@
   import LineChart from './LineChart';
   import BackButton from './BackButton';
 
-  const testfigures = [
-    {
-      name: 'Liikevaihto',
-      value: 23223.00,
-      timespan: 'year',
-      timespanValue: '2017',
-      multiplier: 1,
-    },
-    {
-      name: 'Liikevaihto',
-      value: 23972.00,
-      timespan: 'year',
-      timespanValue: '2016',
-      multiplier: 1,
-    },
-    {
-      name: 'Liikevaihto',
-      value: 12499.00,
-      timespan: 'year',
-      timespanValue: '2015',
-      multiplier: 1,
-    },
-    {
-      name: 'Liikevaihto',
-      value: 11763.00,
-      timespan: 'year',
-      timespanValue: '2014',
-      multiplier: 1,
-    },
-    {
-      name: 'Liikevaihto',
-      value: 12709.00,
-      timespan: 'year',
-      timespanValue: '2013',
-      multiplier: 1,
-    },
-    {
-      name: 'Liikevaihdon muutos, %',
-      value: -3.1,
-      timespan: 'year',
-      timespanValue: '2017',
-      multiplier: 1,
-    },
-    {
-      name: 'Liikevaihdon muutos, %',
-      value: 91.8,
-      timespan: 'year',
-      timespanValue: '2016',
-      multiplier: 1,
-    },
-    {
-      name: 'Liikevaihdon muutos, %',
-      value: 6.3,
-      timespan: 'year',
-      timespanValue: '2015',
-      multiplier: 1,
-    },
-    {
-      name: 'Liikevaihdon muutos, %',
-      value: -7.4,
-      timespan: 'year',
-      timespanValue: '2014',
-      multiplier: 1,
-    },
-    {
-      name: 'Liikevaihdon muutos, %',
-      value: -17.5,
-      timespan: 'year',
-      timespanValue: '2013',
-      multiplier: 1,
-    },
-  ];
+  const testdata = {
+    figures: [
+      {
+        name: 'Liikevaihto',
+        value: 23223.00,
+        timespan: 'year',
+        timespanValue: '2017',
+        multiplier: 1,
+      },
+      {
+        name: 'Liikevaihto',
+        value: 23972.00,
+        timespan: 'year',
+        timespanValue: '2016',
+        multiplier: 1,
+      },
+      {
+        name: 'Liikevaihto',
+        value: 12499.00,
+        timespan: 'year',
+        timespanValue: '2015',
+        multiplier: 1,
+      },
+      {
+        name: 'Liikevaihto',
+        value: 11763.00,
+        timespan: 'year',
+        timespanValue: '2014',
+        multiplier: 1,
+      },
+      {
+        name: 'Liikevaihto',
+        value: 12709.00,
+        timespan: 'year',
+        timespanValue: '2013',
+        multiplier: 1,
+      },
+      {
+        name: 'Liikevaihdon muutos, %',
+        value: -3.1,
+        timespan: 'year',
+        timespanValue: '2017',
+        multiplier: 1,
+      },
+      {
+        name: 'Liikevaihdon muutos, %',
+        value: 91.8,
+        timespan: 'year',
+        timespanValue: '2016',
+        multiplier: 1,
+      },
+      {
+        name: 'Liikevaihdon muutos, %',
+        value: 6.3,
+        timespan: 'year',
+        timespanValue: '2015',
+        multiplier: 1,
+      },
+      {
+        name: 'Liikevaihdon muutos, %',
+        value: -7.4,
+        timespan: 'year',
+        timespanValue: '2014',
+        multiplier: 1,
+      },
+      {
+        name: 'Liikevaihdon muutos, %',
+        value: -17.5,
+        timespan: 'year',
+        timespanValue: '2013',
+        multiplier: 1,
+      },
+    ]
+  };
 
   // object what contains different names
   // map names to buttons
@@ -107,16 +110,19 @@
       const stockData = this.$store.state.stocks.find(single => single.id === this.$route.params.id);
       console.log(stockData)
       // this.allData = stockData;
-      this.allData = testfigures;
+      // currently uses test data
+      this.allData = testdata;
+      this.title = stockData.name;
 
       // create array of names
       const uniquenames = [];
-      for (let i = 0; i < testfigures.length; i += 1) {
-        if (uniquenames.indexOf(testfigures[i].name) === -1) {
-          uniquenames.push(testfigures[i].name);
+      for (let i = 0; i < testdata.figures.length; i += 1) {
+        if (uniquenames.indexOf(testdata.figures[i].name) === -1) {
+          uniquenames.push(testdata.figures[i].name);
         }
       }
       this.names = uniquenames;
+      this.selectedName = uniquenames[0];
     },
 
     data() {
@@ -142,15 +148,17 @@
         selectedData: null,
       };
     },
+    mounted() {
+      this.createData();
+    },
     methods: {
       changeName(name) {
         this.selectedName = name;
         this.createData();
       },
       createData() {
-        console.log('new SelectedData')
         const chartData = Object.create(this.chartDataOptions);
-        const selectedData = this.allData.filter(single => single.name === this.selectedName);
+        const selectedData = this.allData.figures.filter(single => single.name === this.selectedName);
         const labels = [];
         const values = [];
         for (let i = 0; i < selectedData.length; i += 1) {
@@ -161,7 +169,6 @@
         values.reverse();
         chartData.labels = labels;
         chartData.datasets[0].data = values;
-        console.log('Chart data', chartData);
         this.selectedData = chartData;
       }
     }
